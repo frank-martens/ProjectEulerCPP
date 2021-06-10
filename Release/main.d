cd812edb6401a05b468e2f59586742ea254be8d4
Frank Martens <martensfwm@gmail.com>
2021-06-09 17:46:46 -0700

    Moved some files around and added another Euler problem (#3)

---
+++ Release/main.d
@@ -0,0 +1,19 @@
+main.o: ../main.cpp ../ProjectEuler.h ../Math/MathObject.h \
+  ../Math/../peDebug.h ../Math/Multiple3n5.h ../Math/./MathObject.h \
+  ../Math/Fibonacci.h ../Math/LPF.h ../peDebug.h
+
+../ProjectEuler.h:
+
+../Math/MathObject.h:
+
+../Math/../peDebug.h:
+
+../Math/Multiple3n5.h:
+
+../Math/./MathObject.h:
+
+../Math/Fibonacci.h:
+
+../Math/LPF.h:
+
+../peDebug.h:
